5eb0e92eb6ceeef15831a6614665a070ed340f0d
sanazniamati <sanaz.niamati1991@gmail.com>
2022-06-26 13:22:43 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Konva
+React Konva is a JavaScript library for drawing complex canvas graphics using [React](https://reactjs.org/)
+# picture Demo
+
+![Demo](./img.png)
+
+**To create a custom shape with react-konva ?**
+
+[Documentation](https://konvajs.org/docs/react/Custom_Shape.html)
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Stage, Layer, Shape } from "react-konva";
+const App = () => {
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Shape
+          sceneFunc={(context, shape) => {
+            context.beginPath();
+            context.moveTo(20, 50);
+            context.lineTo(220, 80);
+            context.quadraticCurveTo(150, 100, 260, 170);
+            context.closePath();
+            // (!) Konva specific method, it is very important
+            context.fillStrokeShape(shape);
+          }}
+          fill="#00D2FF"
+          stroke="black"
+          strokeWidth={4}
+        />
+      </Layer>
+    </Stage>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
