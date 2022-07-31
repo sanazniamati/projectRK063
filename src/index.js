6b398e7b57bc4af9ac7cb2d35f76c52c2cc9af7c
sanazniamati <sanaz.niamati1991@gmail.com>
2022-07-31 09:41:43 +0430

    2

--- src/index.js
+++ src/index.js
@@ -7,17 +7,30 @@ const App = () => {
       <Layer>
         <Shape
           sceneFunc={(context, shape) => {
+            // {
+            //     // context.beginPath();
+            //     // // context.moveTo(20, 50);
+            //     // context.moveTo(50, 50);
+            //     // // context.lineTo(220, 80);
+            //     // context.lineTo(150, 50);
+            //     // context.lineTo(100, 150);
+            //     // // context.quadraticCurveTo(150, 100, 260, 170);
+            //     // context.closePath();
+            // }
             context.beginPath();
-            context.moveTo(20, 50);
-            context.lineTo(220, 80);
-            context.quadraticCurveTo(150, 100, 260, 170);
+            context.moveTo(150, 50);
+            context.quadraticCurveTo(100, 10, 50, 50);
+            context.lineTo(100, 150);
             context.closePath();
+            context.closePath();
+
             // (!) Konva specific method, it is very important
             context.fillStrokeShape(shape);
           }}
           fill="#00D2FF"
           stroke="black"
           strokeWidth={4}
+          draggable
         />
       </Layer>
     </Stage>
